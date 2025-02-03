d3bf46dca403c169c10080ee081d952d2c5fcd20
Dawid <d.drapinski@proton.me>
2025-02-03 12:40:47 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,21 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	base: "./cube/",
+	base: "/cube/",
 	build: {
 		outDir: "dist",
 		assetsDir: "assets",
 		rollupOptions: {
-			output: {
-				assetFileNames: (assetInfo) => {
-					if (/\.(jpe?g|png|gif|svg|webp)$/.test(assetInfo.name)) {
-						return "assets/images/[name]-[hash][extname]";
-					}
-					return "assets/[name]-[hash][extname]";
-				},
-			},
-		},
+		  output: {
+		    chunkFileNames: 'assets/[name]-[hash].js',
+		    entryFileNames: 'assets/[name]-[hash].js',
+		    assetFileNames: ({ name }) => {
+		      if (/\.(jpe?g|png|gif|svg|webp)$/.test(name)) {
+		        return 'assets/images/[name]-[hash][extname]';
+		      }
+		      return 'assets/[name]-[hash][extname]';
+		    }
+		  }
+		}
 	},
 });
